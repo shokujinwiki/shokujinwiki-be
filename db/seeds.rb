c5c1cf2408a07282975847fc071eb819427d2013
Keita Ito <keitaito@keitaito.net>
2026-03-14 07:15:42 +0900

    feat: create seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,22 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Review.delete_all
+User.delete_all
+
+user_attrs = [
+  { name: "Alice" },
+  { name: "Bob" },
+  { name: "Charlie" }
+]
+
+user_attrs.each do |attrs|
+  user = User.create!(attrs)
+  10.times do |i|
+    Review.create!(
+      content: "#{attrs[:name]} review #{i + 1}",
+      user:
+    )
+  end
+end
