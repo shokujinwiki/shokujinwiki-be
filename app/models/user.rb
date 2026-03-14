1f6c79bc6aed6bf3eba644605ce9881276654588
Keita Ito <keitaito@keitaito.net>
2026-03-14 22:59:12 +0900

    fix: remove cookie methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ApplicationRecord
+  has_secure_password
+  has_many :sessions, dependent: :destroy
   has_many :reviews, dependent: :destroy
 
   validates :name, presence: true
+  validates :email_address, presence: true, uniqueness: true
+
+  normalizes :email_address, with: ->(e) { e.strip.downcase }
 end
