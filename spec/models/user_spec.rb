2da38ffd3d21314d09143dcb88bec113f7bb9c6d
Keita Ito <keitaito@keitaito.net>
2026-03-14 16:47:13 +0900

    feat: tidy tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with valid attributes" do
+    user = build(:user, name: "Alice")
+    expect(user).to be_valid
+  end
+
+  it "is not valid without name" do
+    user = build(:user, name: nil)
+    expect(user).not_to be_valid
+  end
 end
