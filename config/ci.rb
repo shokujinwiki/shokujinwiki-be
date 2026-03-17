6c6a0601452a13d5d067cf5c06383441841d0ff8
Keita Ito <keitaito@keitaito.net>
2026-03-17 17:59:19 +0900

    fix: update CI steps to use RSpec for tests

--- config/ci.rb
+++ config/ci.rb
@@ -7,8 +7,8 @@ CI.run do
 
   step "Security: Gem audit", "bin/bundler-audit"
   step "Security: Brakeman code analysis", "bin/brakeman --quiet --no-pager --exit-on-warn --exit-on-error"
-  step "Tests: Rails", "bin/rails test"
-  step "Tests: Seeds", "env RAILS_ENV=test bin/rails db:seed:replant"
+  step "Tests: RSpec", "bundle exec rspec"
+  step "Tests: Seeds", "bin/rails db:seed:replant"
 
   # Optional: Run system tests
   # step "Tests: System", "bin/rails test:system"
